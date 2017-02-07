ba64fa304c9d1eea2d35b7010ec212ead3c2f7b2
TimotheeFeret <tim.feret@sfr.fr>
2017-02-07 17:20:28 +0100

    appel base par php

--- controleurs/ctrl_inscription.php
+++ controleurs/ctrl_inscription.php
@@ -20,17 +20,26 @@
 			// Connexion à la base échouée.
 			die('Erreur : ' . $e->getMessage());
 		}
-		if(isset($_POST["pseudo"]))
+		if(isset($_POST["username"]))
 		{				
-			$req = $bdd->query('CALL ps_utilisateur_INSERT_utilisateur("Thomas", "aze", 0)');
-
-			// On affiche chaque entrée une à une
-			// while ($donnees = $req->fetch())
-			// {
-				// echo $donnees['ut_pseudo'];
-				// echo $donnees['ut_mdp'];
-				// echo $donnees['ut_droit'];
-			// }
+			// On teste l'existence du pseudo dans la base de données.
+			$req = $bdd->query('CALL ps_utilisateur_GET_utilisateur="'.$_POST['username'].'";');
+			// On stocke le résultat de la requête dans un tableau.
+			$donnees = $req->fetch();
+			// Compte le nombre d'enregistrements contenus dans le tableau.
+			$nombreLigne = count($donnees);
+			if($nombreLigne <= 1)
+			{
+				// Appel de la procédure cui crée un utilisateur.
+				// On fixe le droit à 0, c'est-à-dire que l'utilisateur créé n'a aucun droit spécifique.
+				$bdd->query('CALL ps_utilisateur_INSERT_utilisateur="'.$_POST['username']'", "'.$_POST['password']'";');
+				echo 'Utilisateur créé';
+			}
+			else
+			{
+				echo 'Ce pseudo existe déjà';
+			}
+			echo $nombreLigne;
 			$req->closeCursor(); // Termine le traitement de la requête
 		}
 	}
