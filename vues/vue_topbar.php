ecbf899e5041bc508288cf414cc37ed9265cb379
Simonbaumann <simonbaumann20@gmail.com>
2017-02-09 00:02:48 +0100

    ajout sous-menu topbar

--- vues/vue_topbar.php
+++ vues/vue_topbar.php
@@ -1,20 +1,20 @@
 <div class="topbar">
 	<a class="topbar-logo"></a>
 	<nav class="topbar-menu">
-		<ul></ul>
-		<li><a href="index.php">Accueil</a></li>
-		<ul class ="menu-accordeon">
-			<li><a href="vue_categorie.php">Jonglage</a>
-				<ul>
-					<li><a href="vue_discipline.php">Bolas</a></li>
-					<li><a href="vue_discipline.php">Contact</a></li>
-					<li><a href="vue_discipline.php">Poi</a></li>
-				</ul>
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+			<li class ="dropdown">
+				<a class="dropbtn" href="vue_categorie.php">Jonglage</a>
+				<div class= "dropdown-content">
+					<a href="vue_discipline.php">Bolas</a>
+					<a href="vue_discipline.php">Contact</a>
+					<a href="vue_discipline.php">Poi</a>
+				</div>
 			</li>
-		</ul>
-			<a href="vue_discipline.php">FlipyFlux</a>
-			<li><a href="vue_discipline.php">FlowToys</a>
+			<li><a href="vue_discipline.php">FlipyFlux</a></li>
+			<li><a href="vue_discipline.php">FlowToys</a></li>
 			<li><a href="vue_contact.php">Contact</a></li>
+		</ul>
 	</nav>
 	<!-- if $session is set alors on affiche le pseudo a la place -->
 	<div class="topbar-right">
