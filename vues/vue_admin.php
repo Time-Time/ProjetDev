2edf3d0e714a490f6ec36390501975c3979a5f9b
Simonbaumann <simonbaumann20@gmail.com>
2017-03-10 13:03:51 +0100

    ajout background formulaire admin

--- vues/vue_admin.php
+++ vues/vue_admin.php
@@ -11,41 +11,42 @@
 		</div>
 		<div id="main">
 			<div class="container container-admin">
-				<div class="label-admin">Ajouter une image</div><br/>
-				<div class="form-group">
-					<!-- <form action="../controleurs/ctrl_admin.php" method="post" enctype="multipart/form-data"> -->
-					<form enctype="multipart/form-data">
-						<input type="hidden" name="MAX_FILE_SIZE" value="10000000"/><br/>     <!-- 10 Mo ==>taille maximale du fichier en octets, à mettre avant l'objet input de type file --> 
-						<input style="color:white" type="file" value="choisissez une image" name="fichierCopie" id="fichierCopie"/>
-						<input class="form-control" style="width:300px; margin-left: 216px;" type="text" name="img_desc" id="img_desc" placeholder="Description de l'image"
-							required="required" autofocus="autofocus" maxlength="45"><br/><br/>
-						<input class="btn btn_block" style="width: 200px;" type="button" value="Ajouter l'image" onclick="creerImage()"/><br/>
-					</form>
+				<div class="container-form-admin">
+					<div style="margin-bottom: 0px" class="form-group">
+					<div class="label-admin">Ajouter une image</div>
+						<!-- <form action="../controleurs/ctrl_admin.php" method="post" enctype="multipart/form-data"> -->
+						<form enctype="multipart/form-data">
+							<input type="hidden" name="MAX_FILE_SIZE" value="10000000"/><br/>     <!-- 10 Mo ==>taille maximale du fichier en octets, à mettre avant l'objet input de type file --> 
+							<input style="color:white" type="file" value="choisissez une image" name="fichierCopie" id="fichierCopie"/>
+							<input class="form-control" style="width:300px; margin-left: 216px;" type="text" name="img_desc" id="img_desc" placeholder="Description de l'image"
+								required="required" autofocus="autofocus" maxlength="45"><br/><br/>
+							<input class="btn btn_block" style="width: 200px;" type="button" value="Ajouter l'image" onclick="creerImage()"/><br/>
+						</form>
+					</div>
 				</div>
 				<p style="border-bottom: 0px solid white"></p><br/>
 				<p style="border-bottom: 0px solid white"></p><br/>
-				<div class="label-admin">Ajouter une discipline</div><br/>
-				<div class="form-group">
-					<input class="form-control" style="width:300px;" type="text" name="disc_nom" id="disc_nom" placeholder="Nom de la discipline" required="required" autofocus="autofocus" maxlength="45" onblur="adminVerifDisciplineNom()">
+				<div class="container-form-admin">
+					<div class="label-admin">Ajouter une discipline</div><br/>
+					<div class="form-group">
+						<input class="form-control" style="width:300px;" type="text" name="disc_nom" id="disc_nom" placeholder="Nom de la discipline" required="required" autofocus="autofocus" maxlength="45" onblur="adminVerifDisciplineNom()">
 
-					<select class="form-control listbox-disc" name="disc_image" id="disc_image">
-						<option value="" disabled selected>Image</option>
-						<?php getListeImage() ?>
-					</select>
+						<select class="form-control listbox-disc" name="disc_image" id="disc_image">
+							<option value="" disabled selected>Image</option>
+							<?php getListeImage() ?>
+						</select>
 
-					<select class="form-control listbox-disc" name="disc_categorie" id="disc_categorie">
-						<option value="" disabled selected>Catégorie</option>
-						<?php getListeCategorie() ?>
-					</select>
-
+						<select class="form-control listbox-disc" name="disc_categorie" id="disc_categorie">
+							<option value="" disabled selected>Catégorie</option>
+							<?php getListeCategorie() ?>
+						</select>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control textarea-disc" type="text" name="disc_description" id="disc_description" placeholder="Description de la discipline" required="required" autofocus="autofocus" maxlength="4294967296"></textarea>
+					</div>
+					<input id="btn_creerDiscipline" class="btn btn_block btn_creerdisc" type="button" name="commit" value="Créer la discipline" onclick="creerDiscipline()">
+					</input>
 				</div>
-
-				<div class="form-group">
-					<textarea class="form-control textarea-disc" type="text" name="disc_description" id="disc_description" placeholder="Description de la discipline" required="required" autofocus="autofocus" maxlength="4294967296"></textarea>
-				</div>
-				
-				<input id="btn_creerDiscipline" class="btn btn_block btn_creerdisc" type="button" name="commit" value="Créer la discipline" onclick="creerDiscipline()">
-				</input>
 			</div>
 <?php require_once("vue_footer.php"); ?>
 		</div>
